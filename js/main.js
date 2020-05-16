f1edb1c44b65bb8bd7b1cd447e1d32b86a2f8033
gintaregud <63239813+gintaregud@users.noreply.github.com>
2020-05-16 19:25:35 +0300

    summation

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+var summation = function (num) {
+    let suma = 0;
+    for (let i=1; i<num; i++){
+        suma += i
+    }
+return suma;
+  }
+  
+  
+  
+  
+  
+  
+  console.log(summation(1), 1)
+  console.log(summation(8), 36)
